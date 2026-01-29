3624a1b6675f0ba2c74f16901fa7a6d66176b445
stavros <samaras_st@yahoo.gr>
2026-01-29 14:37:15 +0200

    fix

--- app/routes.ts
+++ app/routes.ts
@@ -3,39 +3,43 @@ export default [
     index("./routes/index.tsx"),
     route("se", "./layouts/service.tsx", [
 
-        ...prefix("technical", [
-            layout("./layouts/technical.tsx", [
-                route("overview/:symbol", "./routes/Technical/overview.tsx"),
-                route("signals", "./routes/Technical/signals.tsx"),
-            ])
-        ]),
-        ...prefix("fundamental", [
-            layout("./layouts/fundamental.tsx", [
-                route("single-analysis", "./routes/Fundamental/single-analysis.tsx", [
-                    route("overview/:symbol", "./routes/Fundamental/single-analysis/overview.tsx"),
-                    route("financials/:symbol", "./routes/Fundamental/single-analysis/financials.tsx"),
-                ]),
-                route("auto-analysis", "./routes/Fundamental/auto-analysis.tsx"),
-            ])
-        ]),
-        ...prefix("sentiment", [
-            layout("./layouts/sentiment.tsx", [
-                index("./routes/Sentiment/Components/SentimentNavigation.tsx")
-            ])
-        ]),
-        ...prefix("contact", [
-            layout("./layouts/contact.tsx", [
-                index("./routes/Contact/contact.tsx")
+        ...(import.meta.env.DEV ? [
+            ...prefix("technical", [
+                layout("./layouts/technical.tsx", [
+                    route("overview/:symbol", "./routes/Technical/overview.tsx"),
+                    route("signals", "./routes/Technical/signals.tsx"),
+                ])
             ]),
-        ]),
+            ...prefix("fundamental", [
+                layout("./layouts/fundamental.tsx", [
+                    route("single-analysis", "./routes/Fundamental/single-analysis.tsx", [
+                        route("overview/:symbol", "./routes/Fundamental/single-analysis/overview.tsx"),
+                        route("financials/:symbol", "./routes/Fundamental/single-analysis/financials.tsx"),
+                    ]),
+                    route("auto-analysis", "./routes/Fundamental/auto-analysis.tsx"),
+                ])
+            ]),
+            ...prefix("sentiment", [
+                layout("./layouts/sentiment.tsx", [
+                    index("./routes/Sentiment/Components/SentimentNavigation.tsx")
+                ])
+            ]),
+            ...prefix("contact", [
+                layout("./layouts/contact.tsx", [
+                    index("./routes/Contact/contact.tsx")
+                ]),
+            ]),
+        ] :
+            []),
         route("financial-history-timeline/:eventCode", "./layouts/FHT.tsx", [
-            // route(":event", "./routes/FHT/eventGraphContainer.tsx")
         ])
     ]),
 
+    ...(import.meta.env.DEV ? [
+        // route("fetchTickerPriceGraph", "./routes/resources/fetchTickerPriceGraph.tsx") 
+        route("fetchTickerPricePoints/:ticker", "./routes/resources/fetchTickerPricePoints.tsx"),
+        route("fetchCryptoLivePrices", "./routes/resources/fetchCryptoLivePrices.tsx"),
+    ] : [])
 
-    // route("fetchTickerPriceGraph", "./routes/resources/fetchTickerPriceGraph.tsx") 
-    route("fetchTickerPricePoints/:ticker", "./routes/resources/fetchTickerPricePoints.tsx"),
-    route("fetchCryptoLivePrices", "./routes/resources/fetchCryptoLivePrices.tsx"),
 
 ] satisfies RouteConfig;

1c45c4742792850dee68de22b28bcd97e7d0f8b2
stavros <samaras_st@yahoo.gr>
2026-01-31 18:20:54 +0200

    change pipeline model

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,11 +16,11 @@ dotenv.config({
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 10 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
+  timeout: 30000,
   /* Configure projects for major browsers */
   projects: [
     {
--- tests/routing.spec.ts
+++ tests/routing.spec.ts
@@ -4,17 +4,17 @@ import { routing } from "./constants/routing";
 
 test.describe.serial("Routing", () => {
 
-
     test("Desktop", async ({ page }, testInfo) => {
         if (testInfo.project.name !== 'chromium') {
             test.skip();
         }
-        console.log(testInfo.project.use.baseURL)
         await testRouting(page, testInfo, routing.desktop);
     });
 
     test("Mobile", async ({ page }, testInfo) => {
-        console.log(testInfo.project.use.baseURL)
+        if (testInfo.project.name !== 'Mobile Safari') {
+            test.skip();
+        }
         await testRouting(page, testInfo, routing.mobile);
     });
 

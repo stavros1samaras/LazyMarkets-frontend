b3815dc4f21c0c943d0bac14137193f1a31c98a0
stavros <samaras_st@yahoo.gr>
2026-01-31 22:23:36 +0200

    fix safari toolbar comp

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,7 +1,7 @@
 export default function Main({ children }: any) {
 
     const m = "ml-4 mt-0 mb-4 mr-4"
-    const h = "h-[calc(100vh-6.25rem)]"
+    const h = `h-[calc(100svh-6.25rem)] h-[calc(100vh-6.25rem)]`
 
     return (
         <main className={`flex flex-col w-[100%] ${h} ${m} p-0 bg-[#dbd9d9] rounded-lg overflow-y-auto scrollbar-hide`}>

46b9c5fc726309a64a3a921b01259666431f30d9
stavros <samaras_st@yahoo.gr>
2026-02-01 13:34:29 +0200

    add crossBrowserStyles for main

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -3,8 +3,11 @@ export default function Main({ children }: any) {
     const m = "ml-4 mt-0 mb-4 mr-4"
     const h = `h-[calc(100svh-6.25rem)] h-[calc(100vh-6.25rem)]`
 
+    const crossBrowserStyles = `scrollbar-hide touch-scroll`
+    const compatHeight = `content-height`
+
     return (
-        <main className={`flex flex-col w-[100%] main-height ${m} p-0 bg-[#dbd9d9] rounded-lg overflow-y-auto scrollbar-hide`}>
+        <main className={`flex flex-col w-[100%] ${compatHeight} ${m} p-0 bg-[#dbd9d9] rounded-lg overflow-y-auto ${crossBrowserStyles}`}>
             {children}
         </main>
     )

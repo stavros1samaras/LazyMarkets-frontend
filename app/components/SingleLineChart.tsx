f4e4fe201857e6192e01af603590fbd4967ba574
stavros <samaras_st@yahoo.gr>
2026-02-25 01:57:01 +0200

    add lading skeleton not ideal

--- app/components/SingleLineChart.tsx
+++ app/components/SingleLineChart.tsx
@@ -5,20 +5,28 @@ import { ResponsiveContainer, XAxis, Tooltip, Area, AreaChart } from "recharts"
 export default function SingleLineChart({ children, data }: any) {
 	return (
 		<>
-			<div className="flex items-center justify-between pb-3">{children}</div>
+			<div className="flex items-center justify-between pb-3 ">{children}</div>
 			<ResponsiveContainer width="100%" height={400}>
 				<AreaChart data={data} margin={{ left: 0, right: 0, top: 0, bottom: 0 }}>
 					<defs>
 						<linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
-							<stop offset="0%" stopColor="#302BCB" stopOpacity={1} />
-							<stop offset="50%" stopColor="#302BCB" stopOpacity={0.6} />
-							<stop offset="100%" stopColor="#302BCB" stopOpacity={0.1} />
+							<stop offset="0%" stopColor="#555fba" stopOpacity={1} />
+							<stop offset="50%" stopColor="#555fba" stopOpacity={0.3} />
+							<stop offset="100%" stopColor="#555fba" stopOpacity={0.05} />
 						</linearGradient>
 					</defs>
 
 					<XAxis dataKey="year" />
 					<Tooltip />
-					<Area type="monotone" dataKey="value" stroke="#3b82f6" strokeWidth={0} dot={false} fill="url(#gradient)" />
+					<Area
+						type="monotone"
+						dataKey="value"
+						stroke="#3b82f6"
+						strokeWidth={0}
+						dot={false}
+						fill="url(#gradient)"
+						isAnimationActive={false}
+					/>
 				</AreaChart>
 			</ResponsiveContainer>
 		</>
--- app/countries/[code]/loading.tsx
+++ app/countries/[code]/loading.tsx
@@ -1,3 +1,20 @@
-export default function loading() {
-	return <p>loading</p>
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
+
+export default function Loading() {
+	return (
+		<>
+			{[...Array(4)].map((_, i) => (
+				<Card key={i} className="w-full">
+					<CardHeader>
+						<Skeleton className="h-4 w-2/3" />
+						<Skeleton className="h-4 w-1/2" />
+					</CardHeader>
+					<CardContent>
+						<Skeleton className="aspect-video w-full" />
+					</CardContent>
+				</Card>
+			))}
+		</>
+	)
 }

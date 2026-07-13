6da762957afa30543978d31f2420f8e5700d7f56
stavros <samaras_st@yahoo.gr>
2026-07-13 19:15:51 +0300

    move CountryDataChartsContainer folder into features folder

---
+++ archived/components/page.tsx
@@ -0,0 +1,21 @@
+// export default async function Countries({ params }: { params: Promise<{ code: string }> }) {
+// 	const countryCode = (await params).code
+
+// 	// const aiData = getGeminiResponse()
+
+// 	return (
+// 		<div className="flex w-full h-full ">
+// 			<DesktopEventSidebar type="events" />
+// 			<Main>
+// 				{/* <CardContainer className="w-auto mb-4" withSection={true}>
+// 					<Ai aiData={aiData} />
+// 				</CardContainer> */}
+// 				<MainContent>
+// 					<Suspense fallback={<Loading />}>
+// 						<CountryDataChartsContainer countryCode={countryCode} />
+// 					</Suspense>
+// 				</MainContent>
+// 			</Main>
+// 		</div>
+// 	)
+// }
--- src/app/countries/[code]/page.tsx
+++ src/app/countries/[code]/page.tsx
@@ -1,21 +1,16 @@
 import DesktopEventSidebar from "../../../_features/countries/navigation/DesktopEventSidebar"
 import Main, { MainContent } from "../../../components/elements/Main"
-import CountryDataChartsContainer from "./CountryDataChartsContainer/CountryChartsContainer"
+import CountryDataChartsContainer from "../../../_features/countries/CountryDataChartsContainer/CountryChartsContainer"
 import Loading from "./loading"
 import { Suspense } from "react"
 
 export default async function Countries({ params }: { params: Promise<{ code: string }> }) {
 	const countryCode = (await params).code
 
-	// const aiData = getGeminiResponse()
-
 	return (
 		<div className="flex w-full h-full ">
 			<DesktopEventSidebar type="events" />
 			<Main>
-				{/* <CardContainer className="w-auto mb-4" withSection={true}>
-					<Ai aiData={aiData} />
-				</CardContainer> */}
 				<MainContent>
 					<Suspense fallback={<Loading />}>
 						<CountryDataChartsContainer countryCode={countryCode} />

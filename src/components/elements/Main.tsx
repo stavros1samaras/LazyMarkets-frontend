a9f15cfeda61fb1a11b3907a5654a3b943637bfb
stavros <samaras_st@yahoo.gr>
2026-07-13 15:26:02 +0300

    refactor Main.tsx

--- src/components/elements/Main.tsx
+++ src/components/elements/Main.tsx
@@ -1,4 +1,6 @@
-export default function Main({ children }: any) {
+import type { ReactNode } from "react"
+
+export default function Main({ children }: { children: ReactNode }) {
 	const m = "ml-4 mt-0 mb-4 mr-4"
 	// const h = `h-[calc(100svh-6.25rem)] h-[calc(100vh-6.25rem)]`
 
@@ -13,8 +15,6 @@ export default function Main({ children }: any) {
 		</main>
 	)
 }
-
-import type { ReactNode } from "react"
 
 export function MainContent({ children }: { children: ReactNode }) {
 	return <section className="grid grid-cols-1 xl:grid-cols-2 gap-4">{children}</section>

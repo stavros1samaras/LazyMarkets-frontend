af084898814c147cc00beb944391335053638731
stavros <samaras_st@yahoo.gr>
2026-07-20 18:19:31 +0300

    add buttons for theme

--- src/components/navigation/Theme.tsx
+++ src/components/navigation/Theme.tsx
@@ -1,10 +1,24 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { ThemeContext } from "@/contexts/ThemeProvider"
+import { Moon, Sun } from "lucide-react"
 import { useContext } from "react"
 
 export default function Theme() {
 	const context = useContext(ThemeContext)
 
-	return <button onClick={context?.toggleTheme}>{context?.theme}</button>
+	return (
+		<>
+			{context?.theme === "light" ? (
+				<Button size="icon" variant="ghost" className="p-0 m-0 text-foreground" onClick={context.toggleTheme}>
+					<Sun className="size-6 p-0" />
+				</Button>
+			) : (
+				<Button size="icon" variant="ghost" className="p-0 m-0 text-foreground" onClick={context?.toggleTheme}>
+					<Moon className="size-6 p-0" />
+				</Button>
+			)}
+		</>
+	)
 }

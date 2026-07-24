a26301c0becc24e78b02d7e955970ab44eb6effd
stavros <samaras_st@yahoo.gr>
2026-07-24 18:07:39 +0300

    fix loading

--- src/app/countries/[code]/loading.tsx
+++ src/app/countries/[code]/loading.tsx
@@ -1,19 +1,22 @@
+import { CardContainer } from "@/components/CardContainer"
 import { Card, CardContent, CardHeader } from "../../../components/ui/card"
 import { Skeleton } from "../../../components/ui/skeleton"
 
 export default function Loading() {
 	return (
 		<>
-			{[...Array(4)].map((_, i) => (
-				<Card key={i} className="w-full bg-component-background">
-					<CardHeader>
-						<Skeleton className="h-4 w-2/3 bg-primary-foreground" />
-						<Skeleton className="h-4 w-1/2 bg-primary-foreground" />
-					</CardHeader>
-					<CardContent>
-						<Skeleton className="aspect-video w-full  bg-primary-foreground" />
-					</CardContent>
-				</Card>
+			{[...Array(8)].map((_, i) => (
+				<CardContainer key={i} className="bg-component-background">
+					<Card className="w-full bg-component-background">
+						<CardHeader>
+							<Skeleton className="h-4 w-2/3 bg-primary-foreground" />
+							<Skeleton className="h-4 w-1/2 bg-primary-foreground" />
+						</CardHeader>
+						<CardContent>
+							<Skeleton className="aspect-video w-auto  bg-primary-foreground" />
+						</CardContent>
+					</Card>
+				</CardContainer>
 			))}
 		</>
 	)

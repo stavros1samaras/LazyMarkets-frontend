27d041029232fec844c18c6241c42294524a0dbd
stavros <samaras_st@yahoo.gr>
2026-07-26 21:29:53 +0300

    add Text with asChild prop in DesktopHeader

--- src/components/navigation/DesktopHeader.tsx
+++ src/components/navigation/DesktopHeader.tsx
@@ -11,56 +11,62 @@ export default function DesktopHeader() {
 		<Header className="w-auto h-13 mx-4 my-4 bg-component-background rounded-lg justify-between px-2">
 			<MainNavigationMenu>
 				<Text className={className}>SAMOKO</Text>
-
-				<Link
-					href={
-						process.env.NODE_ENV === "production"
-							? "/se/financial-hishrefry-timeline/TrmpTrrffShck"
-							: "/se/technical/overview/BTC-USD"
-					}
-					prefetch={false}
-					data-testid="techical"
-				>
-					<Text className={className}>Techical</Text>
-				</Link>
-
-				<Link
-					href={
-						process.env.NODE_ENV === "production"
-							? "/se/financial-hishrefry-timeline/TrmpTrrffShck"
-							: "/se/fundamental/single-analysis/overview/AAPL"
-					}
-					prefetch={false}
-					data-testid="fundamental"
-				>
-					<Text className={className}>Fundamental</Text>
-				</Link>
-
-				<Link
-					className="hidden sm:block"
-					href={process.env.NODE_ENV === "production" ? "/se/financial-hishrefry-timeline/TrmpTrrffShck" : "/se/sentiment"}
-					prefetch={false}
-					data-testid="sentiment"
-				>
-					<Text className={className}>Sentiment</Text>
-				</Link>
-
-				<Link href="/se/financial-hishrefry-timeline/TrmpTrrffShck" prefetch={false} data-testid="FHT">
-					<Text className={className}>FHT</Text>
-				</Link>
-
-				<Link
-					className="hidden sm:block"
-					href={process.env.NODE_ENV === "production" ? "/se/financial-hishrefry-timeline/TrmpTrrffShck" : "/se/contact"}
-					prefetch={false}
-					data-testid="contact"
-				>
-					<Text className={className}>Contact</Text>
-				</Link>
-
-				<Link className="hidden sm:block" href="/countries/GR" data-testid="contact">
-					<Text className={className}>Countries</Text>
-				</Link>
+				<Text asChild className={className}>
+					<Link
+						href={
+							process.env.NODE_ENV === "production"
+								? "/se/financial-hishrefry-timeline/TrmpTrrffShck"
+								: "/se/technical/overview/BTC-USD"
+						}
+						prefetch={false}
+						data-testid="techical"
+					>
+						Techical
+					</Link>
+				</Text>
+				<Text asChild className={className}>
+					<Link
+						href={
+							process.env.NODE_ENV === "production"
+								? "/se/financial-hishrefry-timeline/TrmpTrrffShck"
+								: "/se/fundamental/single-analysis/overview/AAPL"
+						}
+						prefetch={false}
+						data-testid="fundamental"
+					>
+						Fundamental
+					</Link>
+				</Text>
+				<Text asChild className={className}>
+					<Link
+						className="hidden sm:block"
+						href={process.env.NODE_ENV === "production" ? "/se/financial-hishrefry-timeline/TrmpTrrffShck" : "/se/sentiment"}
+						prefetch={false}
+						data-testid="sentiment"
+					>
+						Sentiment
+					</Link>
+				</Text>
+				<Text asChild className={className}>
+					<Link href="/se/financial-hishrefry-timeline/TrmpTrrffShck" prefetch={false} data-testid="FHT">
+						FHT
+					</Link>
+				</Text>
+				<Text asChild className={className}>
+					<Link
+						className="hidden sm:block"
+						href={process.env.NODE_ENV === "production" ? "/se/financial-hishrefry-timeline/TrmpTrrffShck" : "/se/contact"}
+						prefetch={false}
+						data-testid="contact"
+					>
+						Contact
+					</Link>
+				</Text>
+				<Text asChild className={className}>
+					<Link className="hidden sm:block" href="/countries/GR" data-testid="contact">
+						Countries
+					</Link>
+				</Text>
 			</MainNavigationMenu>
 			<ThemeButton />
 		</Header>

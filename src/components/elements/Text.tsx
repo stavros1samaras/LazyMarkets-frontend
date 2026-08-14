ead73be73301c4e4f514fcc2713738897e5ba01b
stavros <samaras_st@yahoo.gr>
2026-08-14 12:57:35 +0300

    add cn in Text

--- src/components/elements/Text.tsx
+++ src/components/elements/Text.tsx
@@ -21,7 +21,11 @@ export default function Text({ asChild = false, as = "p", className, children, .
 
 	const Tag = as
 	return (
-		<Tag className={`text-foreground ${className} responsive-${as}`} {...props}>
+		<Tag
+			// className={`text-foreground ${className} responsive-${as}`}
+			className={cn("text-foreground", className, `responsive-${as}`)}
+			{...props}
+		>
 			{children}
 		</Tag>
 	)

c18f3c3eceec6e7c59b325a93d57e074a7d5f900
stavros <samaras_st@yahoo.gr>
2026-09-20 14:37:34 +0300

    add translation for countries dropdown

--- src/_features/countries/sections/countries-header/components/CountrySelector.tsx
+++ src/_features/countries/sections/countries-header/components/CountrySelector.tsx
@@ -2,7 +2,9 @@
 
 import { useRouter } from "next/navigation"
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from "@/components/ui/select"
+import Text from "@/components/elements/Text"
 import { COUNTRIES } from "@/_features/countries/config"
+import { useTranslation } from "react-i18next"
 
 interface CountrySelectorProps {
 	className?: string
@@ -10,6 +12,7 @@ interface CountrySelectorProps {
 
 export default function CountrySelector({ className }: CountrySelectorProps) {
 	const router = useRouter()
+	const { t } = useTranslation()
 
 	return (
 		<Select
@@ -18,14 +21,16 @@ export default function CountrySelector({ className }: CountrySelectorProps) {
 			}}
 		>
 			<SelectTrigger size="sm" className={className}>
-				<SelectValue placeholder="Select a country" />
+				<SelectValue placeholder={t(`Select a country`)} />
 			</SelectTrigger>
 			<SelectContent className="bg-background border-ring text-foreground" position="popper" side="bottom">
 				<SelectGroup>
-					<SelectLabel>COUNTRIES</SelectLabel>
+					<SelectLabel>
+						<Text as="span">COUNTRIES</Text>
+					</SelectLabel>
 					{COUNTRIES.map((country, index) => (
-						<SelectItem value={country.code} key={index}>
-							{country.name}
+						<SelectItem value={country.code} key={index} className="focus:bg-select-item">
+							<Text as="span">{country.name}</Text>
 						</SelectItem>
 					))}
 				</SelectGroup>
